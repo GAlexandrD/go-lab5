f6da4616ec58e4c1208c9dcd268134bd486cb2e6
suxarik <suxarik312004gor@gmail.com>
2023-05-28 16:56:35 +0300

    added balancer implementation

--- cmd/lb/balancer.go
+++ cmd/lb/balancer.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"net/http"
 	"time"
 
@@ -14,21 +16,46 @@ import (
 )
 
 var (
-	port = flag.Int("port", 8090, "load balancer port")
+	port       = flag.Int("port", 8090, "load balancer port")
 	timeoutSec = flag.Int("timeout-sec", 3, "request timeout time in seconds")
-	https = flag.Bool("https", false, "whether backends support HTTPs")
+	https      = flag.Bool("http", false, "whether backends support HTTPs")
 
 	traceEnabled = flag.Bool("trace", false, "whether to include tracing information into responses")
 )
 
+type serverType struct {
+	dst             string
+	dataTransferred int
+	isWorking       bool
+}
+
 var (
-	timeout = time.Duration(*timeoutSec) * time.Second
-	serversPool = []string{
-		"server1:8080",
-		"server2:8080",
-		"server3:8080",
+	timeout     = time.Duration(*timeoutSec) * time.Second
+	serversPool = []serverType{
+		{
+			dst:             "server1:8080",
+			dataTransferred: 0,
+			isWorking:       false,
+		}, {
+			dst:             "server2:8080",
+			dataTransferred: 0,
+			isWorking:       false,
+		}, {
+			dst:             "server3:8080",
+			dataTransferred: 0,
+			isWorking:       false,
+		},
 	}
 )
+
+type healthCheckerInterface interface {
+	health(dst string) bool
+}
+
+type Balancer struct {
+	pool []serverType
+	hc   healthCheckerInterface
+}
 
 func scheme() string {
 	if *https {
@@ -37,7 +64,47 @@ func scheme() string {
 	return "http"
 }
 
-func health(dst string) bool {
+func SetupBalancer() *Balancer{
+	balancer := &Balancer{
+		pool: serversPool,
+		hc:   &healthChecker{},
+	}
+	balancer.runChecker()
+	balancer.Start()
+	return balancer
+}
+
+func (b *Balancer) runChecker() {
+	for i := range b.pool {
+		server := &b.pool[i]
+		go func() {
+			b.checkHealth(server)
+			for range time.Tick(10 * time.Second) {
+				b.checkHealth(server)
+			}
+		}()
+	}
+}
+
+func (b *Balancer) Start() {
+	frontend := httptools.CreateServer(*port, http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		index, err := b.getIndex()
+		if err != nil {
+			log.Println(err.Error())
+		} else {
+			b.forward(&b.pool[index], rw, r)
+		}
+	}))
+
+	log.Println("Starting load balancer...")
+	log.Printf("Tracing support enabled: %t", *traceEnabled)
+	frontend.Start()
+	signal.WaitForTerminationSignal()
+}
+
+type healthChecker struct{}
+
+func (hc *healthChecker) health(dst string) bool {
 	ctx, _ := context.WithTimeout(context.Background(), timeout)
 	req, _ := http.NewRequestWithContext(ctx, "GET",
 		fmt.Sprintf("%s://%s/health", scheme(), dst), nil)
@@ -51,23 +118,29 @@ func health(dst string) bool {
 	return true
 }
 
-func forward(dst string, rw http.ResponseWriter, r *http.Request) error {
+var cnt int = 0
+
+func (b *Balancer) forward(server *serverType, rw http.ResponseWriter, r *http.Request) error {
 	ctx, _ := context.WithTimeout(r.Context(), timeout)
 	fwdRequest := r.Clone(ctx)
 	fwdRequest.RequestURI = ""
-	fwdRequest.URL.Host = dst
+	fwdRequest.URL.Host = server.dst
 	fwdRequest.URL.Scheme = scheme()
-	fwdRequest.Host = dst
-
+	fwdRequest.Host = server.dst
 	resp, err := http.DefaultClient.Do(fwdRequest)
 	if err == nil {
+
+		// count length of server response and save it
+		length := headerLength(resp.Header) + int(resp.ContentLength)
+		server.dataTransferred += length
+
 		for k, values := range resp.Header {
 			for _, value := range values {
 				rw.Header().Add(k, value)
 			}
 		}
 		if *traceEnabled {
-			rw.Header().Set("lb-from", dst)
+			rw.Header().Set("lb-from", server.dst)
 		}
 		log.Println("fwd", resp.StatusCode, resp.Request.URL)
 		rw.WriteHeader(resp.StatusCode)
@@ -78,7 +151,7 @@ func forward(dst string, rw http.ResponseWriter, r *http.Request) error {
 		}
 		return nil
 	} else {
-		log.Printf("Failed to get response from %s: %s", dst, err)
+		log.Printf("Failed to get response from %s: %s", server.dst, err)
 		rw.WriteHeader(http.StatusServiceUnavailable)
 		return err
 	}
@@ -86,24 +159,42 @@ func forward(dst string, rw http.ResponseWriter, r *http.Request) error {
 
 func main() {
 	flag.Parse()
+	SetupBalancer()
+}
 
-	// TODO: Використовуйте дані про стан сервреа, щоб підтримувати список тих серверів, яким можна відправляти ззапит.
-	for _, server := range serversPool {
-		server := server
-		go func() {
-			for range time.Tick(10 * time.Second) {
-				log.Println(server, health(server))
-			}
-		}()
+func (b *Balancer) checkHealth(server *serverType) {
+	isWorking := b.hc.health(server.dst)
+	server.isWorking = isWorking
+	log.Println(server, b.hc.health(server.dst))
+}
+
+func (b *Balancer) getIndex() (int, error) {
+	index := 0
+	minData := math.MaxInt
+	for i := range b.pool {
+		if !b.pool[i].isWorking {
+			continue
+		}
+		curData := b.pool[i].dataTransferred
+		if curData < minData {
+			index = i
+			minData = curData
+		}
 	}
+	if !b.pool[index].isWorking {
+		return 0, errors.New("There are no servers available")
+	}
+	return index, nil
+}
 
-	frontend := httptools.CreateServer(*port, http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		// TODO: Рееалізуйте свій алгоритм балансувальника.
-		forward(serversPool[0], rw, r)
-	}))
-
-	log.Println("Starting load balancer...")
-	log.Printf("Tracing support enabled: %t", *traceEnabled)
-	frontend.Start()
-	signal.WaitForTerminationSignal()
+func headerLength(header http.Header) int {
+	var str string
+	for key, values := range header {
+		for _, value := range values {
+			str += fmt.Sprintf("%s: %s\n", key, value)
+		}
+	}
+	byteSlice := []byte(str)
+	byteCount := len(byteSlice)
+	return byteCount
 }
