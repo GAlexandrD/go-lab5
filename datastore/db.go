845676ed1fcf900d25097db3848936acc91f2155
suxarik <suxarik312004gor@gmail.com>
2023-06-01 11:50:45 +0300

    add segments implementation

--- datastore/db.go
+++ datastore/db.go
@@ -9,34 +9,46 @@ import (
 	"path"
 	"path/filepath"
 	"strconv"
+	"sync"
 )
 
 const outFileName = "current-data"
 
 var ErrNotFound = fmt.Errorf("record does not exist")
 
+type mergeItem struct {
+	path   string
+	offset int64
+}
+
+type mergeIndex map[string]mergeItem
+
 type hashIndex map[string]int64
 
 type Db struct {
-	dir string
-	out *os.File
-	outPath string
+	out       *os.File
+	dir       string
+	outPath   string
 	outOffset int64
-
-	index hashIndex
-	segments []hashIndex
+	mu        sync.Mutex
+	limit     int64
+	index     hashIndex
+	segments  []hashIndex
 }
 
-func NewDb(dir string, segLength int64) (*Db, error) {
+func NewDb(dir string, segmLimit int64) (*Db, error) {
 	outputPath := filepath.Join(dir, outFileName)
 	f, err := os.OpenFile(outputPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0o600)
 	if err != nil {
 		return nil, err
 	}
+
 	db := &Db{
 		outPath: outputPath,
 		out:     f,
+		dir:     dir,
 		index:   make(hashIndex),
+		limit:   segmLimit,
 	}
 	err = db.recover()
 	if err != nil && err != io.EOF {
@@ -48,26 +60,53 @@ func NewDb(dir string, segLength int64) (*Db, error) {
 const bufSize = 8192
 
 func (db *Db) recover() error {
-	input, err := os.Open(db.outPath)
+	_, err := os.Stat(db.outPath)
+	if err == nil {
+		index, offset, err := recoverFile(db.outPath)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		db.index = index
+		db.outOffset = offset
+	}
+	
+	for i := 0; ; i++ {
+		_, err := os.Stat(db.getSPath(i))
+		if err != nil {
+			break
+		}
+		index, _, err := recoverFile(db.getSPath(i))
+		if err != nil && err != io.EOF {
+			return err
+		}
+		db.segments = append(db.segments, index)
+	}
+	return nil
+}
+
+func recoverFile(path string) (hashIndex, int64, error) {
+	input, err := os.Open(path)
 	if err != nil {
-		return err
+		return nil, 0, err
 	}
 	defer input.Close()
 
+	index := make(hashIndex)
+	var offset int64
 	var buf [bufSize]byte
 	in := bufio.NewReaderSize(input, bufSize)
 	for err == nil {
 		var (
 			header, data []byte
-			n int
+			n            int
 		)
 		header, err = in.Peek(bufSize)
 		if err == io.EOF {
 			if len(header) == 0 {
-				return err
+				return index, offset, err
 			}
 		} else if err != nil {
-			return err
+			return nil, 0, err
 		}
 		size := binary.LittleEndian.Uint32(header)
 
@@ -80,29 +119,36 @@ func (db *Db) recover() error {
 
 		if err == nil {
 			if n != int(size) {
-				return fmt.Errorf("corrupted file")
+				return nil, 0, fmt.Errorf("corrupted file")
 			}
 
 			var e entry
 			e.Decode(data)
-			db.index[e.key] = db.outOffset
-			db.outOffset += int64(n)
+			index[e.key] = offset
+			offset += int64(n)
 		}
 	}
-	return err
+	return index, offset, err
 }
 
 func (db *Db) Close() error {
+
 	return db.out.Close()
 }
 
 func (db *Db) Get(key string) (string, error) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+	outPath := db.outPath
 	position, ok := db.index[key]
 	if !ok {
-		return "", ErrNotFound
+		outPath, position, ok = db.getFromSegments(key)
+		if !ok {
+			return "", ErrNotFound
+		}
 	}
 
-	file, err := os.Open(db.outPath)
+	file, err := os.Open(outPath)
 	if err != nil {
 		return "", err
 	}
@@ -121,17 +167,63 @@ func (db *Db) Get(key string) (string, error) {
 	return value, nil
 }
 
+func (db *Db) getFromSegments(key string) (string, int64, bool) {
+	var (
+		outPath  string
+		position int64
+		ok       bool = false
+	)
+	if len(db.segments) == 0 {
+		return "", 0, false
+	}
+	for i := len(db.segments) - 1; i >= 0; i-- {
+		position, ok = db.segments[i][key]
+		if ok {
+			outPath = db.getSPath(i)
+			break
+		}
+	}
+	return outPath, position, ok
+}
+
 func (db *Db) Put(key, value string) error {
+	db.mu.Lock()
 	e := entry{
 		key:   key,
 		value: value,
 	}
 	n, err := db.out.Write(e.Encode())
-	if err == nil {
-		db.index[key] = db.outOffset
-		db.outOffset += int64(n)
+	if err != nil {
+		db.mu.Unlock()
+		return err
+	}
+	db.index[key] = db.outOffset
+	db.outOffset += int64(n)
+	if db.outOffset > db.limit {
+		err = db.addSegment()
+	} else {
+		db.mu.Unlock()
 	}
 	return err
+}
+
+func (db *Db) addSegment() error {
+	db.Close()
+	newSegmentPath := db.getSPath(len(db.segments))
+	err := os.Rename(db.outPath, newSegmentPath)
+	if err != nil {
+		return err
+	}
+	f, err := os.OpenFile(db.outPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0o600)
+	if err != nil {
+		return err
+	}
+	db.out = f
+	db.outOffset = 0
+	db.segments = append(db.segments, db.index)
+	db.index = make(hashIndex)
+	db.mu.Unlock()
+	return nil
 }
 
 func (db *Db) getSPath(index int) string {
